e35c9658f45a85eb8f09f88b3f33400e1c61dc6f
KuberPujar <kuberp1992@gmail.com>
2021-02-02 16:49:23 +0530

    second commit

--- src/main/java/com/juno/restservices/SpringBootProjectsApplication.java
+++ src/main/java/com/juno/restservices/SpringBootProjectsApplication.java
@@ -2,7 +2,7 @@ package com.juno.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//dffdffd
 @SpringBootApplication
 public class SpringBootProjectsApplication {
 
