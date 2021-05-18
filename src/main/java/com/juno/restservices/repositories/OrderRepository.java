c946dfcff8cd7efe56e629cf1445f157a046f459
KuberPujar <kuberp1992@gmail.com>
2021-05-18 21:42:18 +0530

    commit

--- src/main/java/com/juno/restservices/repositories/OrderRepository.java
+++ src/main/java/com/juno/restservices/repositories/OrderRepository.java
@@ -1,9 +1,10 @@
 package com.juno.restservices.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.juno.restservices.entities.Order;
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long>,CrudRepository<Order, Long>{
 
 }
